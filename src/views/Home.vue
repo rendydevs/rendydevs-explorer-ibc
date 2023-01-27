ae87b50e205b2ba0dbf6723d2f42061377a2bb05
rendydevs <rendyalviardianshah@gmail.com>
2023-01-27 21:03:11 +0700

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,7 +7,7 @@
         <h1
           class="text-primary display-4 font-weight-bolder d-none d-md-block"
         >
-          Ping Dashboard<small class="flow-left">Beta</small>
+          rendydevs explorer<small class="flow-left">Powered By PingPub</small>
         </h1>
       </div>
     </b-link>
